37692566809d4eaf217b270ad954c0b525d8328a
sicacodoso <494905160@qq.com>
2023-04-19 10:22:30 +0800

    day1基础

---
+++ day01_黑马c++/day01_黑马c++/C_语言增强的地方.cpp
@@ -0,0 +1,21 @@
+
+
+
+void test()
+{
+int const a= 10;
+int *p =  (int*)&a;
+*p = 20;
+}
+
+enum season
+{
+	SPR = 0,
+	SUM,
+	AUT,
+	WIN,
+};
+void test1()
+{
+
+}
---
+++ day01_黑马c++/day01_黑马c++/命名空间.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//iostream提供了一个叫命名空间的东西 std是标准命名空间
+
+//方式二 
+using std::cout;
+using std::endl;
+#if 0
+int main(void) {
+
+    
+    //方式一
+    std:: cout << "hello word" <<std::endl;
+
+}
+#endif
