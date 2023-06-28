2d71de8fd8b1b962449e8f355782f7fdb0a79ae4
sicacodoso <494905160@qq.com>
2023-06-28 17:57:43 +0800

    1

---
+++ day c++primer/test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{	
+	char v1 = {}, v2 = {};
+	cin >> v1 >> v2;
+	cout << "the sum of " << v1 << " And " << v2 << " is " << v1 + v2 << endl;
+
+	return 0;
+}
